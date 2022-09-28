34b3421d5c74756a4fa6c69c7af4b5ea44bc2355
Musasizi Kenneth <kennymusasizi@hotmail.com>
2022-09-28 09:37:30 +0300

    [lesson] Arithmetic Operations

---
+++ week4/week4.py
@@ -0,0 +1,45 @@
+# integers
+# print(10)
+# print(2)
+# print(-2)
+
+# floats -  floating-point numbers
+# print(10.2)
+# print(2.4)
+# print(.4)
+# print(-.6)
+# print(1.0)
+
+# Octadecimal Base
+# print(0o127)
+
+# Hexadecimal
+# print(0x123)
+
+
+# print(3e8)
+# print(3e-2)
+
+# name
+# Bread
+# Unit Cost
+# Quantity
+# Cost
+
+# print(8+2)
+# print(8-2)
+# print(8*2)
+# print(8/2)
+
+# print(8 % 3)
+# print(round(8 / 3, 0))
+# print(8 // 3 * 3/ 5)
+# print(2**3)
+
+
+print(1_000_000)
+
+unit_cost = 6000
+quantity = 5
+total_cost = quantity * unit_cost
+# print(total_cost)
