0f84d92eafa29ebfb2b3e9c8368b9bf2eaccd4d3
Musasizi Kenneth <kennymusasizi@hotmail.com>
2022-09-30 10:06:36 +0300

    [excersise] conditionals practice

--- week4/ex1.py
+++ week4/ex1.py
@@ -1,55 +1,55 @@
 # Question 1
-x = 5
-y = 10
-z = 8
+# x = 5
+# y = 10
+# z = 8
 
-print(x > y)
-print(y > z)
+# print(x > y) #False
+# print(y > z) #False
 
 
-# Question 2
-x, y, z = 5, 10, 8
+# # Question 2
+# x, y, z = 5, 10, 8
 
-print(x > z)
-print((y - 5) == x)
+# print(x > z) #False
+# print((y - 5) == x) #True
 
-# Question 3
-x, y, z = 5, 10, 8
-x, y, z = z, y, x
+# # Question 3
+# x, y, z = 5, 10, 8
+# x, y, z = z, y, x
 
-print(x > z)
-print((y - 5) == x)
+# print(x > z)#True
+# print((y - 5) == x)#False
 
-# Question 4
-x = 10
+# # Question 4
+# x = 10
 
-if x == 10:
-    print(x == 10)
-if x > 5:
-    print(x > 5)
-if x < 10:
-    print(x < 10)
-else:
-    print("else")
+# if x == 10:
+#     print(x == 10) #True
+# if x > 5:
+#     print(x > 5) #True
+# if x < 10:
+#     print(x < 10) #else
+# else:
+#     print("else")
 
-# Question 5
-x = "1"
+# # Question 5
+# x = "1"
 
-if x == 1:
-    print("one")
-elif x == "1":
-    if int(x) > 1:
-        print("two")
-    elif int(x) < 1:
-        print("three")
-    else:
-        print("four")
-if int(x) == 1:
-    print("five")
-else:
-    print("six")
+# if x == 1:
+#     print("one")
+# elif x == "1":
+#     if int(x) > 1:
+#         print("two")
+#     elif int(x) < 1:
+#         print("three")
+#     else:
+#         print("four")
+# if int(x) == 1:
+#     print("five")
+# else:
+#     print("six")
 
-# Question 6
+# # Question 6
 x = 1
 y = 1.0
 z = "1"
---
+++ week4/functions.py
@@ -0,0 +1,14 @@
+
+
+# def greet_user(great_statement, name):
+#     print(great_statement, name)
+
+
+# greet_user("Howdy", "Ken")
+
+
+def add_numbers(num1, num2):
+    return num1 + num2
+
+
+print(add_numbers(3, 6))
