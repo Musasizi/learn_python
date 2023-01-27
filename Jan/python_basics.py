8695648a40c0ac9a2431a6ec0418fdca1c2fc964
Musasizi Kenneth <kennymusasizi@hotmail.com>
2023-01-27 10:39:59 +0300

    more about literals

---
+++ Jan/python_basics.py
@@ -0,0 +1,29 @@
+# Operations in python
+# print(1+2)
+# print(1-2)
+# print(4/2)
+# print(4*2)
+# print(4%2)
+# print(3%2)
+# print(0/4)
+
+# Literals in python
+# Numeric literals
+print(200)
+print(10.4)
+
+# String Literals
+print('Apofia')
+print('I may be any sentence')
+
+# Boolean literals
+print(True)
+print(False)
+
+# Special literals
+print(None)
+
+# Collection Literals
+print([1, 2, 3])
+print(['Jane', 2, 3, False])
+print(["UCU", "MUK", "HARVARD", ['Jane', 2, 3, False]])
---
+++ Jan/variables.py
@@ -0,0 +1,34 @@
+# Declaring variables
+# name = 'Jane Doe'
+# print(name)
+
+
+firstNumber = 1
+secondNumber = 4
+
+sum_answer = first_number + second_number
+diff_answer = first_number - second_number
+product_answer = first_number * second_number
+quotient_answer = first_number / second_number
+modulus_answer = first_number % second_number
+
+# print(sum_answer)
+# print(diff_answer)
+# print(product_answer)
+# print(quotient_answer)
+# print(modulus_answer)
+
+
+is_it_raining = False
+print(is_it_raining)
+
+PI = 3.14
+PLANKS_CONSTANT = 2.6899
+
+# Variable naming convention
+
+# snake_case
+# camelCaseWriting
+# CapsCase
+# PythonBasics
+# MACRO_CASE
