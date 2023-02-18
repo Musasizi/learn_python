f9b2aa4d5692e4fe0af30a8c16a0709a9a2bec93
Musasizi Kenneth <kennymusasizi@hotmail.com>
2023-02-18 16:28:53 +0300

    loops

---
+++ Jan/loops.py
@@ -0,0 +1,67 @@
+# Collections
+
+list_of_hostels = ['High Field', 'Sky Courts',
+                   'Premuim', 'Pameja', 'David Ark']
+
+
+# print(list_of_hostels[-1])
+# print(list_of_hostels[-2])
+# print(list_of_hostels[1:-2])
+# print(list_of_hostels[2:-2])
+# print(list_of_hostels[5])
+
+# print(list_of_hostels[2], "and", list_of_hostels[1])
+# print(list_of_hostels[1:4])
+# print(len(list_of_hostels))
+# print(list_of_hostels[len(list_of_hostels) - 1])
+
+
+# for hostel in list_of_hostels:
+#     print(hostel, "🧵")
+
+# print(range(1,10))
+# for i in range(1, 10):
+#     print(i)
+
+
+# program to calculate the sum of numbers
+# until the user enters zero
+
+
+# total = 0
+
+# number = int(input('Enter a number: '))
+
+# # add numbers until number is zero
+# while number != 0:
+#     total += number    # total = total + number
+
+#     # take integer input again
+#     number = int(input('Enter a number: '))
+
+
+# print('total =', total)
+
+
+# n = 10
+
+# # use pass inside if statement
+# if n > 10:
+#     pass
+
+# print('Hello')
+
+
+# Lambda or Anonymous functions
+
+# def greet_user ():
+#     print("Hello, ")=p[]\=
+
+greet_user = lambda user, country: print("Hello,", user, "from", country)
+
+# greet_user("Ken", "Uganda")
+
+x = ['ab', 'cd']
+for i in x:
+ x.append(i.upper())
+print(x)
