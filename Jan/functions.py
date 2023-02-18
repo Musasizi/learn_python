385ca0f19e93b802f0b24f0f07a920c9992217fb
Musasizi Kenneth <kennymusasizi@hotmail.com>
2023-02-18 16:28:45 +0300

    revisiting functions

---
+++ Jan/functions.py
@@ -0,0 +1,28 @@
+def new_print(text, emoji):
+    print(text, emoji * 3)
+
+
+# function definition
+def sum_of_numbers(first_number, second_number):
+    return first_number + second_number
+
+
+# function call
+print(sum_of_numbers(3, 10), "🌎")
+new_print(sum_of_numbers(5, 11), "🙌")
+new_print(sum_of_numbers(38, 2), "🙌")
+new_print(sum_of_numbers(8, 5), "🙌")
+new_print(sum_of_numbers(39889, 1), "🙌")
+
+
+def product_of_numbers(first_number, second_number):
+    new_print(first_number * second_number,  "🙌")
+
+
+product_of_numbers(4, 3)
+
+
+new_print("Hello, UCU ", "🧵")
+new_print("Hello, World ", "🌎")
+new_print("Hello, Mukono ", "😁")
+new_print("Hello, Kauga ", "🌹")
