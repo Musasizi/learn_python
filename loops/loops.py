443bc65833f9b5c988b4e451186136e9565996d7
Musasizi Kenneth <kennymusasizi@hotmail.com>
2023-10-26 11:18:33 +0300

    Create loops.py

---
+++ loops/loops.py
@@ -0,0 +1,31 @@
+
+# For loop
+# for i in range(0,5):
+#     print("This is item number", i+1, "in the cart!🛒")
+#     print("Packing item......")
+
+
+# while loop
+# n = 0
+
+# while n<5:
+#     print("N is less than 5 and the value is", n)
+#     n +=1
+
+# cart_items = ["Sugar", "Soap", "Mangoes", "Bread"]
+#              -4        -3          -2        -1
+# print(cart_items[-1])
+
+# for item in cart_items:
+#     if item == "Sugar":
+#         print("Yaah this is sugar!🍡🍡🍡🍡🍡")
+#     else:
+#         print("This is ",item)
+    
+age = int(input("Please enter your age! "))
+while age <18:
+    print("Go to bed 🛌")
+    age = int(input("We need you to be above 18: "))
+    
+print("Congs 🎊🎊🎊🎊 ")
+    
