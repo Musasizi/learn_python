a1c5da24aacca668652832ac6f4a5977699d2fbc
Musasizi Kenneth <kennymusasizi@hotmail.com>
2023-11-02 10:42:42 +0300

    functions

---
+++ Extras/more_functions.py
@@ -0,0 +1,19 @@
+
+
+def factorial(x):
+    if x ==1:
+        return 1
+    else:
+        return(x * factorial(x-1))
+
+print(factorial(3))
+
+
+
+students = ['q','e','er']
+
+print('e' in students)
+
+people = {"name": "k", "age" :3}
+
+print(people["age"])
