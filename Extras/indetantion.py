0f21e4d8471baaa81fd1ce140d2f8aab91a4128b
Musasizi Kenneth <kennymusasizi@hotmail.com>
2024-01-10 12:16:16 +0300

    Create indetantion.py

---
+++ Extras/indetantion.py
@@ -0,0 +1,11 @@
+def main():
+    print("Welcome to the Indentation Race!")
+for i in range(5):
+print("Current number:", i)
+if i % 2 == 0:
+print("Even number.")
+else:
+print("Odd number.")
+print("Race completed!")
+
+main()
