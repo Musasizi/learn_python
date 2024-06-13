88b24b3d6886f01f586112f4fca5cf8fd3d99b4a
Musasizi Kenneth <24485588+Musasizi@users.noreply.github.com>
2024-06-13 09:35:50 -0500

    More operations

---
+++ TRINITY2024/hello.py
@@ -0,0 +1,27 @@
+
+first_number = int(input("Enter first number of your choice: "))
+second_number =int(input("Enter second number of your choice: "))
+
+Sum =(first_number+second_number)
+print("the Sum is,",Sum)
+
+product= first_number*second_number
+print("the product is,",product)
+
+quotient = first_number/second_number
+print("the quotient is,",quotient)
+
+modulus = second_number%first_number
+print("the modulus is,",modulus)
+
+difference= second_number-first_number
+print("the difference is,",difference)
+
+age= int(input("enter a number of your choice: "))
+print("your age is,", age)
+
+#calculating age
+birth_year= int(input("Enter the birth year: "))
+current_year =int(input("Enter the current year: "))
+age= current_year-birth_year
+print("the age is,", age)
